ea8b7449bf4dfaddb3172c37674ac9ca28300513
yalh76 <yalh@yahoo.com>
2025-02-12 02:47:18 +0100

    Fix and improving

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,7 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+php_exec_as_app() {
+	ynh_hide_warnings ynh_exec_as_app php${php_version} --define apc.enable_cli=1 "$@"
+}
